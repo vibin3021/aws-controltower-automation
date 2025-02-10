950c70f1ae883f9de8f0787bef583993393659da
VIBIN B G <165311826+vibin3021@users.noreply.github.com>
2025-02-10 20:00:52 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
