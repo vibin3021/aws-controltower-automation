0349889a5cc15508de096f2df76936c7c54d431e
VIBIN B G <165311826+vibin3021@users.noreply.github.com>
2025-02-11 21:58:59 +0530

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
